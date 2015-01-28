7d26ecceb3fff032e2750fdb0a58427b5db445b6
Endre Karlson <endre.karlson@hp.com>
2015-01-29 00:18:32 +0100

    Fix how signing and verification is done

--- src/jwt.nim
+++ src/jwt.nim
@@ -8,12 +8,15 @@ type
     InvalidToken = object of Exception
 
     JWT* = object
+        headerB64: string
+        claimsB64: string
         header*: JOSEHeader
         claims*: TableRef[string, Claim]
         signature*: string
 
 export claims
 export jose
+
 
 
 proc splitToken(s: string): seq[string] =
@@ -27,13 +30,17 @@ proc splitToken(s: string): seq[string] =
 proc toJWT*(s: string): JWT =
     var parts = splitToken(s)
     let
-      headerNode = parseJson(decodeUrlSafe(parts[0]))
-      payloadNode = parseJson(decodeUrlSafe(parts[1]))
+      headerB64 = parts[0]
+      claimsB64 = parts[1]
+      headerJson = parseJson(decodeUrlSafe(headerB64))
+      claimsJson = parseJson(decodeUrlSafe(claimsB64))
       signature = decodeUrlSafe(parts[2])
 
     result = JWT(
-        header: headerNode.toHeader(),
-        claims: payloadNode.toClaims(),
+        headerB64: headerB64,
+        claimsB64: claimsB64,
+        header: headerJson.toHeader(),
+        claims: claimsJson.toClaims(),
         signature: signature
     )
 
@@ -51,36 +58,54 @@ proc toJWT*(node: JsonNode): JWT =
 # Encodes the raw signature hex to b64url
 proc signatureToB64(token: JWT): string =
   assert token.signature != nil
-  return encodeUrlSafe(token.signature)
+  result = encodeUrlSafe(token.signature)
 
 
-# Returns the signature as a string
-proc sign*(token: JWT, secret: var string): string =
+proc loaded*(token: JWT): string =
+  token.headerB64 & "." & token.claimsB64
+
+
+proc parsed*(token: JWT): string =
+  result = token.header.toBase64 & "." & token.claims.toBase64
+
+
+# Signs a string with a secret
+proc signString*(toSign: string, secret: var string): string =
   var
-    signature:  array[32, uint8]
+    signature: array[32, uint8]
     sigsize: cuint
-    toSign = token.header.toBase64 & "." & token.claims.toBase64
-
   discard hmac.HMAC(hmac.EVP_sha256(), addr(secret[0]), 8, toSign.cstring, toSign.len.cint, cast[ptr char](addr signature), addr sigsize)
-
-  result = $signature.map((i: uint8) => (toHex(BiggestInt(i), 2)))
+  result = join(signature.map((i: uint8) => (toHex(BiggestInt(i), 2))), "")
 
 
 # Verify that the token is not tampered with
+proc verifySignature*(data: string, signature: string, secret: var string): bool =
+  let dataSignature = signString(data, secret)
+  result = dataSignature == signature
+
+
+proc sign*(token: var JWT, secret: var string) =
+  assert token.signature == nil
+  token.signature = signString(token.parsed, secret)
+
+
+# Verify a token typically an incoming request
 proc verify*(token: JWT, secret: var string): bool =
-  let signature = token.sign(secret)
-  result = token.signature == signature
+  result = verifySignature(token.loaded, token.signature, secret)
 
 
 proc toString*(token: JWT): string =
   token.header.toBase64 & "." & token.claims.toBase64 & "." & token.signatureToB64
 
+
 proc `$`*(token: JWT): string =
   token.toString
+
 
 proc `%`*(token: JWT): JsonNode =
   let s = $token
   %s
+
 
 when isMainModule:
     # Load a token from json
@@ -100,13 +125,12 @@ when isMainModule:
     var
       token = JWT(
         claims: claimsJson.toClaims,
-        header: headerJson.toHeaders
+        header: headerJson.toHeader
       )
       secret = "secret"
 
     # Sign and verify
-    token.signature = token.sign(secret)
-    assert token.verify(secret) == true
+    token.sign(secret)
 
     let b64Token = $token
     token = b64Token.toJWT
